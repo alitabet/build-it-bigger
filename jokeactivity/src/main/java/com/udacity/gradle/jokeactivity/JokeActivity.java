4f8f97ef2a3496b0828e60901b728ba2ca8bb389
Ali Thabet <alitabet@gmail.com>
2015-12-23 16:42:01 +0300

    Implement java and Android libraries

--- jokeactivity/src/main/java/com/udacity/gradle/jokeactivity/JokeActivity.java
+++ jokeactivity/src/main/java/com/udacity/gradle/jokeactivity/JokeActivity.java
@@ -1,33 +1,24 @@
-package com.udacity.gradle.builditbigger;
+package com.udacity.gradle.jokeactivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
-import com.example.JokeFactory;
-import com.udacity.gradle.jokeactivity.JokeActivity;
+public class JokeActivity extends AppCompatActivity {
 
+    public static String JOKE_KEY = "Joke key";
 
-public class MainActivity extends ActionBarActivity {
-
-    private JokeFactory mJokeFactory; // joke factory to retrieve jokes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mJokeFactory = new JokeFactory();
+        setContentView(R.layout.activity_joke);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_joke, menu);
         return true;
     }
 
@@ -45,14 +36,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void tellJoke(View view){
-        JokeFactory joker = new JokeFactory();
-        Intent intent = new Intent(this, JokeActivity.class);
-        intent.putExtra(JokeActivity.JOKE_KEY, joker.getJoke());
-        startActivity(intent);
-        Toast.makeText(this, mJokeFactory.getJoke(), Toast.LENGTH_LONG).show();
-    }
-
 
 }
