0ea9948dcb415bff4c43057079dcfef15f00842d
Ali Thabet <alitabet@gmail.com>
2016-01-04 16:49:22 +0300

    Add comments to espresso test

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityEspressoTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityEspressoTest.java
@@ -16,7 +16,11 @@ import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.hamcrest.Matchers.not;
 
 /**
- * Created by alitabet on 12/28/15.
+ * Android Instrumentation Test to check
+ * if AsyncTask return a non-null or
+ * non-empty joke string
+ *
+ * @author Ali K Thabet
  */
 @RunWith(AndroidJUnit4.class)
 public class MainActivityEspressoTest {
@@ -27,7 +31,10 @@ public class MainActivityEspressoTest {
 
     @Test
     public void asyncShouldReturnJoke() {
+        // click the joke retrieve button
         onView(withId(R.id.button_get_joke)).perform(click());
+        // check that the TextView text in jokesactivity
+        // is not empty or not null
         onView(withId(R.id.joke_text_view)).check(matches(withText(not(isEmptyOrNullString()))));
     }
 }
